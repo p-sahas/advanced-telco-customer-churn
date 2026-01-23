4f8674a7fa5fdfcfc3eae10ff76fa3e5d9bab793
p-sahas <eap.sahas@gmail.com>
2026-01-23 20:41:39 +0530

    data ingestion pipeline

--- pipelines/data_pipeline.py
+++ pipelines/data_pipeline.py
@@ -28,13 +28,34 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
 from config import get_data_paths, get_columns, get_missing_values_config, get_outlier_config, get_binning_config, get_encoding_config, get_scaling_config, get_splitting_config
 
 scalling_config = get_scaling_config()
+data_paths = get_data_paths()
 
 def data_pipeline(file_path: str = 'data/raw/Telco-Customer-Churn.csv') -> pd.DataFrame:
     # Step 1: Data Ingestion
     logger.info("Starting data ingestion.")
-    ingestor = DataIngestorCSV()
-    df = ingestor.ingest(file_path)
-    logger.info("Data ingestion completed.")
+    artifacts_dir = os.path.join(os.path.dirname(__file__), '..', data_paths['data_artifacts_dir'])
+    x_train_path = os.path.join('artifacts', 'data', 'X_train.csv')
+    x_test_path = os.path.join('artifacts', 'data', 'X_test.csv')
+    y_train_path = os.path.join('artifacts', 'data', 'Y_train.csv')
+    y_test_path = os.path.join('artifacts', 'data', 'Y_test.csv')
+    
+    if os.path.exists(x_train_path) and \
+        os.path.exists(x_test_path) and \
+        os.path.exists(y_train_path) and \
+        os.path.exists(y_test_path):
+        
+        X_train = pd.read_csv(x_train_path)
+        X_test = pd.read_csv(x_test_path)
+        Y_train = pd.read_csv(y_train_path)
+        Y_test = pd.read_csv(y_test_path) 
+
+
+    os.makedirs(file_path['data_artifacts_dir'], exist_ok=True)
+    if not os.path.exists('temp_imputed.csv'):
+        ingestor = DataIngestorCSV()
+        df = ingestor.ingest(file_path)
+        print(f"loaded data shape : {df.shape}")
+        logger.info("Data ingestion completed.")
 
     # Step 2: Handle Missing Values
     logger.info("Handling missing values.")
@@ -81,5 +102,16 @@ def data_pipeline(file_path: str = 'data/raw/Telco-Customer-Churn.csv') -> pd.Da
     X_train, X_test, Y_train, Y_test = splitter.split_data(df, 'Churn')
     logger.info("Data splitting completed.")
 
+    # Create directories and save splits
+    os.makedirs('artifacts/data', exist_ok=True)
+    X_train.to_csv(x_train_path, index=False)
+    X_test.to_csv(x_test_path, index=False)
+    Y_train.to_csv(y_train_path, index=False)
+    Y_test.to_csv(y_test_path, index=False)
+
+    print(f'X train size : {X_train.shape}')
+    print(f'X test size : {X_test.shape}')
+    print(f'Y train size : {Y_train.shape}')
+    print(f'Y test size : {Y_test.shape}')
     # logger.info("Data pipeline completed.")
     # return df
