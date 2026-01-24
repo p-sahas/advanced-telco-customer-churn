164f5abda34467d93aff8909758fc7dfc2d402e6
p-sahas <eap.sahas@gmail.com>
2026-01-24 17:03:21 +0530

    xgboost model

--- src/model_building.py
+++ src/model_building.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
-from sklearn.model_selection import KFold, StratifiedKFold, GridSearchCV, cross_val_score
 
 class BaseModelBuilder(ABC):
     def __init__(self, model_name: str, **kwargs):
@@ -44,3 +43,37 @@ class LogisticRegressionModelBuilder(BaseModelBuilder):
         self.model = LogisticRegression(**self.params)
         return self.model
     
+class DecisionTreeModelBuilder(BaseModelBuilder):
+    def __init__(self, model_name, **kwargs):
+        default_params = {
+            'criterion': 'gini',
+            'max_depth': None,
+            'max_features': None,
+            'min_samples_split': 2,
+            'min_samples_leaf': 1,
+            'ccp_alpha': np.float64(0.00041608656377458515),
+            'random_state': 42
+        }
+        default_params.update(kwargs)
+        super().__init__('DecisionTree', **default_params)
+
+    def build_model(self):
+        self.model = DecisionTreeClassifier(**self.params)
+        return self.model
+
+class XGBoostModelBuilder(BaseModelBuilder):
+    def __init__(self, model_name, **kwargs):
+        default_params = {
+            'n_estimators': 100,
+            'max_depth': 9,
+            'learning_rate': 0.05,
+            'colsample_bytree': 0.6,
+            'gamma': 0.2,
+            'random_state': 42
+        }  
+        default_params.update(kwargs)
+        super().__init__('XGBoost', **default_params)
+
+    def build_model(self):
+        self.model = XGBClassifier(**self.params)
+        return self.model
